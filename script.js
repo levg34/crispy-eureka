0d2e8b4b1e945f7438025322935610176fb5779c
levg34 <levg34@users.noreply.github.com>
2017-12-05 15:33:22 +0100

    working with lines movements between points

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var X_MAX = $(window).width()
 var Y_MAX = $(window).height()
 var pl = []
+var timeInterval = 10
 
 function loadImage() {
 	Y_MAX -= $('#img').height()
@@ -31,23 +32,62 @@ function displayThing(point) {
 	//console.log(point)
 }
 
+function lineBetween(p1,p2) {
+	var a = 0
+	var b = 0
+	
+	var x1 = p1.x
+	var y1 = p1.y
+	var x2 = p2.x
+	var y2 = p2.y
+	
+	a = (y1-y2)/(x1-x2)
+	b = (x1*y2-x2*y1)/(x1-x2)
+	
+	return function(x) {
+		return a * x + b
+	}
+}
+
+var trace = []
+
+function animateLine(p1,p2) {
+	var f = lineBetween(p1,p2)
+	trace = []
+
+	if (p1.x<p2.x) {
+		for (var x=p1.x;x<p2.x;x++) {
+			trace.push({x:x,y:f(x)})
+		}
+	} else {
+		for (var x=p1.x;x>p2.x;x--) {
+			trace.push({x:x,y:f(x)})
+		}
+	}
+
+	interval = setInterval(displayNext,timeInterval)
+}
+
 function displayNext() {
-	var point = pl.pop()
+	var point = trace.shift()
 	if (point&&point.x&&point.y) {
 		displayThing(point)
-	} else if (pl.length==0) {
-		//$('#img').hide()
-		displayThing({x:X_MAX,y:Y_MAX})
+	} else if (trace.length==0) {
 		clearInterval(interval)
+		p1 = p2
+		p2 = pl.shift()
+		if (p2&&p2.x&&p2.y) {
+			animateLine(p1,p2)
+		}
 	}
 }
 
 pl = pointsList(15)
+pl.push({x:X_MAX,y:Y_MAX})
 
-var interval = setInterval(displayNext,1500)
+var p1 = {x:0,y:0}
+var p2 = pl.shift()
 
-/*pointsList(15).forEach(function(point) {
-	setTimeout(function() {
-		displayThing(point)
-	}, 1500)
-})*/
+animateLine(p1,p2)
+
+var interval = setInterval(displayNext,timeInterval)
