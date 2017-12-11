ff495e558a0b6a3831c0afb43e239e5b3c8ecec7
levg34 <levg34@users.noreply.github.com>
2017-12-11 08:54:49 +0100

    catmull-roll

--- script.js
+++ script.js
@@ -1,93 +1,55 @@
-var X_MAX = $(window).width()
-var Y_MAX = $(window).height()
-var pl = []
-var timeInterval = 10
+var windowWidth = $(window).width()
+var windowHeight = $(window).height()
+var imageWidth = $('#img').width()
+var imageHeight = $('#img').height()
 
-function loadImage() {
-	Y_MAX -= $('#img').height()
-	X_MAX -= $('#img').width()
-}
-
-loadImage()
-
-function randBetween(a,b) {
-	return Math.floor(Math.random()*(b+1))+a
-}
-
-function createPoint() {
-	return {x:randBetween(0,X_MAX),y:randBetween(0,Y_MAX)}
-}
-
-function pointsList(nbPts) {
-	var list = []
-	for (var i=0;i<nbPts;++i) {
-		list.push(createPoint())
-	}
-	return list
-}
+var xMin = 0
+var yMin = 0
+var xMax = windowWidth - imageWidth
+var yMax = windowHeight - imageHeight
 
 function displayThing(point) {
 	$('#img').css('top', point.y)
 	$('#img').css('left', point.x)
-	//console.log(point)
 }
 
-function lineBetween(p1,p2) {
-	var a = 0
-	var b = 0
-	
-	var x1 = p1.x
-	var y1 = p1.y
-	var x2 = p2.x
-	var y2 = p2.y
-	
-	a = (y1-y2)/(x1-x2)
-	b = (x1*y2-x2*y1)/(x1-x2)
-	
-	return function(x) {
-		return a * x + b
+var catm = (p0, p1, m0, m1) => (t) => {
+	return {
+		x: (2*t*t*t-3*t*t+1)*p0.x + (t*t*t-2*t*t+t)*m0.x + (-2*t*t*t+3*t*t)*p1.x + (t*t*t-t*t)*m1.x,
+		y: (2*t*t*t-3*t*t+1)*p0.y + (t*t*t-2*t*t+t)*m0.y + (-2*t*t*t+3*t*t)*p1.y + (t*t*t-t*t)*m1.y
 	}
 }
-
-var trace = []
-
-function animateLine(p1,p2) {
-	var f = lineBetween(p1,p2)
-	trace = []
-
-	if (p1.x<p2.x) {
-		for (var x=p1.x;x<p2.x;x++) {
-			trace.push({x:x,y:f(x)})
-		}
-	} else {
-		for (var x=p1.x;x>p2.x;x--) {
-			trace.push({x:x,y:f(x)})
-		}
-	}
-
-	interval = setInterval(displayNext,timeInterval)
+var cerp = (a, b, c, d) => {
+	var m0 = !!a ? {x: c.x-a.x, y: c.y-a.y} : {x: c.x-b.x, y: c.y-b.y}
+	var m1 = !!d ? {x: d.x-b.x, y: d.y-b.y} : {x: c.x-b.x, y: c.y-b.y} 
+	return catm(b, c, m0, m1)
 }
+var rand = (min, max) => () => {return Math.floor(Math.random() * (max + 1)) + min}
+var randX = rand(xMin, xMax)
+var randY = rand(yMin, yMax)
 
-function displayNext() {
-	var point = trace.shift()
-	if (point&&point.x&&point.y) {
-		displayThing(point)
-	} else if (trace.length==0) {
-		clearInterval(interval)
-		p1 = p2
-		p2 = pl.shift()
-		if (p2&&p2.x&&p2.y) {
-			animateLine(p1,p2)
-		}
-	}
+var t = 0
+var i = 0
+var iMax = 15
+var pA = {x: randX(), y: randY()}
+var pB = {x: randX(), y: randY()}
+var pC = {x: randX(), y: randY()}
+var pD = {x: randX(), y: randY()}
+animate = () => {
+	t += 1
+	t %= 100
+	
+	if (t === 0) {
+		++i
+		pA = {x: pB.x, y: pB.y}
+		pB = {x: pC.x, y: pC.y}
+		pC = {x: i >= iMax ? xMax : pD.x, y: i >= iMax ? yMax : pD.y}
+		pD = {x: i >= iMax-1 ? xMax : randX(), y: i >= iMax-1 ? yMax : randY()}
+    }
+
+    if (i <= iMax) {
+		displayThing(cerp(pA, pB, pC, pD)(t/100))
+		requestAnimationFrame(animate)    	
+    }
 }
-
-pl = pointsList(15)
-pl.push({x:X_MAX,y:Y_MAX})
-
-var p1 = {x:0,y:0}
-var p2 = pl.shift()
-
-animateLine(p1,p2)
-
-var interval = setInterval(displayNext,timeInterval)
+requestAnimationFrame(animate)
